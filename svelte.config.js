4a8cd928b842fddadd1c97bb8e82ee975d40de84
Jaeun Park <park.jaeun.jan@gmail.com>
2024-09-27 01:28:30 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -7,10 +7,11 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-
 			// Set the base path for GitHub Pages
-			base: process.argv.includes('dev') ? '' : '/visualization-portfolio'
+			base: process.env.NODE_ENV === 'production' ? '/visualization-portfolio' : ''
+		},
+		prerender: {
+			default: true // Automatically prerender all pages
 		}
 	}
 };
